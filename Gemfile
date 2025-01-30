77151b382fc30be3756b76b1884b9b4504543fc5
Zuadinho <alancostacs@hotmail.com>
2025-01-29 21:50:03 -0300

    Finalização

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source "https://rubygems.org"
 
 ruby "3.0.0"
 
+#--------------GEM PROS ICONES-------------#
+gem "font-awesome-sass"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.4", ">= 7.1.4.1"
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "armazenamentos#index"
   resources :produtos, except: [:show]
   resources :compras, except: [:show]
   resources :armazenamentos, only: [:index, :edit, :update]
